75afb9f6b0d2c68fa04d2b1cf3673916e608c5aa
praveen kumar <41241820+mepky@users.noreply.github.com>
2018-07-23 03:36:31 +0530

    Update logistic_regression.py

--- classification_folder/logistic_regression.py
+++ classification_folder/logistic_regression.py
@@ -1,5 +1,7 @@
-# Logistic Regression
+#praveen kumar
+#Indian institute of information technology
 
+# Logistic Regression
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
